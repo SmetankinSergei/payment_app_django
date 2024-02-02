739087cc2b85fb7720e64d79285893c4bbc7c6c4
Smetankin <smet-eb@yandex.ru>
2024-02-02 18:32:52 +0300

    in progress

--- payments/views.py
+++ payments/views.py
@@ -1,15 +1,41 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
 from payments.templatetags.payments_tags import get_payments
 from payments.templatetags.requisites_tags import get_requisites, get_search_result
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 
 def show_payments(request):
+    payments_list = get_payments()
+    requisites_list = get_requisites()
+
+    payments_page = request.GET.get('payments_page', 1)
+    requisites_page = request.GET.get('requisites_page', 1)
+    items_per_page = 30
+
+    paginator_payments = Paginator(payments_list, items_per_page)
+    paginator_requisites = Paginator(requisites_list, items_per_page)
+
+    try:
+        payments = paginator_payments.page(payments_page)
+    except PageNotAnInteger:
+        payments = paginator_payments.page(1)
+    except EmptyPage:
+        payments = paginator_payments.page(paginator_payments.num_pages)
+
+    try:
+        requisites = paginator_requisites.page(requisites_page)
+    except PageNotAnInteger:
+        requisites = paginator_requisites.page(1)
+    except EmptyPage:
+        requisites = paginator_requisites.page(paginator_requisites.num_pages)
+
     context = {
         'title': 'payments',
-        'payments': get_payments(),
-        'requisites': get_requisites(),
+        'payments': payments,
+        'requisites': requisites,
     }
     return render(request, 'payments/start_page.html', context)
 
