1298a77ea6d4bd15ae824a9892af3ac2d4d35084
Smetankin <smet-eb@yandex.ru>
2024-02-03 09:26:23 +0300

    done

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'payments_db',
         'USER': 'postgres',
         'HOST': '127.0.0.1',
-        'PORT': '5433',
+        'PORT': '5432',
         'PASSWORD': '1111'
     }
 }
